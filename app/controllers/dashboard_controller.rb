a9b9c40d37c43231a61cb186aa04d0ae4a2dbfd6
Juan Pablo <juanpablogast@gmail.com>
2021-06-30 20:31:12 -0400

    cantidad de ventas mensuales

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    @sales_mensuales = Sale.group_by_monthd(date: :asc).count
+    # @column_data = Sales.coffee_type.keys.map do |type|
+    #   { name: type.capitalize, data: Expense.where(type: type).group_by_week(:date).sum(:amount)}
+    #   end 
+    @sales_grouped_by_month = Sale.group_by_month(:date, last: 13 ).order(date: :asc).sum(:price)
+    @sales_grouped_by_number_of_sales = Sale.group_by_month(:date, last: 13 ).order(date: :asc).count
+
+
+
   end
 end
